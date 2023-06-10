fd9d2ca419e98de0cb796143173ab467322052f5
Ajith chandra sekar <114874198+ajithilan@users.noreply.github.com>
2023-06-10 23:20:03 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -25,7 +25,7 @@ const store = configureStore({
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router basename='/Ecommerce'>
+      <Router>
         <App />
       </Router>
     </Provider>
